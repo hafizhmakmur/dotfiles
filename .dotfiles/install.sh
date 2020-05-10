3942aad97ec695807fcba204f0d95411495be7ae
mcmoor <hafizhmakmur@gmail.com>
2020-05-10 14:03:48 +0700

    Modif install.sh

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -2,3 +2,4 @@
 ln -sf $HOME/.dotfiles/.vimrc $HOME/.vimrc
 ln -sf $HOME/.dotfiles/.bashrc $HOME/.bashrc
 ln -sf $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -sf $HOME/.dotfiles/plugins.vim $HOME/.vim/plugins.vim
